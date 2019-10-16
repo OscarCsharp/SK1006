28102070956f3ea3e3f2a5d9d8fa0abdd890de9c
Oscar Shaun Csharp <oscarndlovu01@gmail.com>
2019-10-16 12:33:21 +0200

    ClientProject

---
+++ Client/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Learners()
+        {
+            return View();
+        }
+
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        public IActionResult Enquiries()
+        {
+            return View();
+        }
+        public IActionResult Modules()
+        {
+            return View();
+        }
+
+        public IActionResult Approved()
+        {
+            return View();
+        }
+
+        public IActionResult Applicants()
+        {
+            return View();
+        }
+
+    }
+}
